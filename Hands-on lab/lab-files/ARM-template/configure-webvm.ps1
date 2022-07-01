621afe0fa570cadcb2a394e9682146e8e6e821e7
kalyani7744 <83349665+kalyani7744@users.noreply.github.com>
2022-07-01 10:58:02 +0530

    Update configure-webvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
@@ -186,6 +186,7 @@ Start-Process -file 'C:\vscode.exe' -arg '/VERYSILENT /SUPPRESSMSGBOXES /LOG="C:
 
 
 #Autologin
+$Username = "demouser"
 $Pass = "$adminPassword"
 $RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
 Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
