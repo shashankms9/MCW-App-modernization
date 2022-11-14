f3111dac16b8a3121e2cc74d9f08a97dfb87cb03
kalyani7744 <83349665+kalyani7744@users.noreply.github.com>
2022-11-14 12:45:31 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -19,7 +19,7 @@ function Wait-Install {
         Start-Sleep -Seconds 1
     }
 }
-$branchName = "main"
+$branchName = "microsoft-app-modernization-v2"
 # Install App Service Migration Assistant
 Wait-Install
 Write-Host "Installing App Service Migration Assistant..."
