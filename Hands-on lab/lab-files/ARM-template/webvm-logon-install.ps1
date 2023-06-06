714ae0e9fea2c58ba17b75141b10026b6476ac29
Sumit Malik <sumitmalik51@gmail.com>
2023-06-06 13:33:02 +0530

    Update webvm-logon-install.ps1

--- Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -130,6 +130,9 @@ else{
 
 sleep 50
 
+Invoke-AzVMRunCommand -ResourceGroupName "hands-on-lab-$DeploymentID" -Name 'SqlServer2008' -CommandId 'RunPowerShellScript' -ScriptPath -ScriptString "(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
+Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process"
+
 CloudlabsManualAgent setStatus
 
 CloudLabsManualAgent Start
