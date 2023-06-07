6ac40e37c3000adbe8956a33ab41029f97562738
kalyani7744 <83349665+kalyani7744@users.noreply.github.com>
2023-06-07 14:04:18 +0530

    Update configure-sqlvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
@@ -104,7 +104,9 @@ Setup-Sql
 
 
 $env:chocolateyUseWindowsCompression = 'true'
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:chocolateyIgnoreRebootDetected = 'true'
+$env:chocolateyVersion = '1.4.0'
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco install dotnetfx -y -force
 
