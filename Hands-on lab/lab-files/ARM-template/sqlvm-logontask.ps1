391d95081489932437e24e55485fbadde34e4384
Sumit Malik <sumitmalik51@gmail.com>
2023-06-08 13:28:46 +0530

    Update sqlvm-logontask.ps1

--- Hands-on lab/lab-files/ARM-template/sqlvm-logontask.ps1
+++ Hands-on lab/lab-files/ARM-template/sqlvm-logontask.ps1
@@ -1,25 +1,2 @@
 (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi', 'C:\DataMigrationAssistant.msi')
 Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
-
-$WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("http://go.microsoft.com/fwlink/?LinkID=2093437","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
-    sleep 5
-    
-    Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
-    sleep 5
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Portal.lnk")
-    $Shortcut.TargetPath = """C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"""
-    $argA = """https://portal.azure.com"""
-    $Shortcut.Arguments = $argA 
-    $Shortcut.Save()
-    
-     $env:chocolateyUseWindowsCompression = 'true'
-    $env:chocolateyIgnoreRebootDetected = 'true'
-    $env:chocolateyVersion = '1.4.0'
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    choco feature enable -n allowGlobalConfirmation
-    
-    refreshenv
- 
-choco install azure-data-studio -y
