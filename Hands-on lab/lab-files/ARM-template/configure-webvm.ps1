18d5c853955e03b4c1dd532b47e4f2176ca6a096
suraj-only-git <141921174+suraj-only-git@users.noreply.github.com>
2024-02-01 19:34:22 +0530

    Update configure-webvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
@@ -2,27 +2,33 @@ param (
     [Parameter(Mandatory = $true)]
     [string]
     $AzureUserName,
-
+ 
     [string]
     $AzurePassword,
-
-    [string]
-    $ODLID,
-
-    [string]
-    $InstallCloudLabsShadow,
-
-    [string]
-    $DeploymentID,
-    
+ 
     [string]
     $AzureTenantID,
-  
+ 
     [string]
     $AzureSubscriptionID,
-  
+ 
     [string]
-    $adminPassword
+    $ODLID,
+ 
+    [string]
+    $DeploymentID,
+ 
+    [string]
+    $vmAdminUsername,
+ 
+    [string]
+    $adminPassword,
+ 
+    [string]
+    $trainerUserName,
+ 
+    [string]
+    $trainerUserPassword
 )
 
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
@@ -32,16 +38,21 @@ $trainerUserName="trainer"
 $trainerUserPassword="$adminPassword"
 
 
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+ 
 #Import Common Functions
 $path = pwd
 $path=$path.Path
 $commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
 . $commonscriptpath
+#Installing Modern VM Validator
+InstallModernVmValidator
+ 
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
 
-# Enable Embedded shadow
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
 
-CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
 az provider register --namespace "Microsoft.LoadTestService"
 
 #Reset VM password to update it to random password, it is a custom image based VM
